ddaff7360358d3529999a1f179c5188b1075ffd8
StormCoderWorld <131701367+StormCoderWorld@users.noreply.github.com>
2024-09-14 00:55:18 -0400

    Update sim-mode-defs.js
    Slightly cooler ATL

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -1288,7 +1288,7 @@ ENV_DEFS[SIM_MODE_NorthAtlantic].SST = {
         offSeasonPolarTemp: -14,
         peakSeasonPolarTemp: 4,
         offSeasonTropicsTemp: 27,
-        peakSeasonTropicsTemp: 30
+        peakSeasonTropicsTemp: 29
 
     }
 };
@@ -1298,7 +1298,7 @@ ENV_DEFS[SIM_MODE_SouthAtlantic].SST = {
         offSeasonPolarTemp: -18,
         peakSeasonPolarTemp: -6,
         offSeasonTropicsTemp: 27,
-        peakSeasonTropicsTemp: 29.2
+        peakSeasonTropicsTemp: 29.4
     }
 };
 ENV_DEFS[SIM_MODE_Mediterranean].SST = {
