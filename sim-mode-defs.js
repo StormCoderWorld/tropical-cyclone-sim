b29397bdfe6b328d57f2bd8b58432c80f4e734f3
StormCoderWorld <131701367+StormCoderWorld@users.noreply.github.com>
2025-07-31 23:37:41 -0400

    Update sim-mode-defs.js
    Reverted sine and cosine curves

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -1173,7 +1173,7 @@ ENV_DEFS.defaults.SST = {
         let pspt = u.modifiers.peakSeasonPolarTemp;
         let ostt = u.modifiers.offSeasonTropicsTemp;
         let pstt = u.modifiers.peakSeasonTropicsTemp;
-        let t = lerp(map(s,-1.15,0.5,ospt,pspt), map(s,-1,1.15,ostt,pstt),h);
+        let t = lerp(map(s,-1,1,ospt,pspt), map(s,-1,1,ostt,pstt),h);
         return t+anom;
     }, 
 
